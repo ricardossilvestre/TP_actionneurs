9aa513b0be6771f93944eb2bf3a9d5f6fdc6a3bd
Alvaro <alvarorodsan@msn.com>
2022-11-25 20:49:27 +0100

    Added final coments to the code

--- TP_actionneurs/Core/Src/main.c
+++ TP_actionneurs/Core/Src/main.c
@@ -37,21 +37,18 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define UART_TX_BUFFER_SIZE 64
-#define UART_RX_BUFFER_SIZE 1
-#define CMD_BUFFER_SIZE 64
-#define MAX_ARGS 9
-// LF = line feed, saut de ligne
-#define ASCII_LF 0x0A
-// CR = carriage return, retour chariot
-#define ASCII_CR 0x0D
-// DEL = delete
-#define ASCII_DEL 0x7F
+#define UART_TX_BUFFER_SIZE 64				///< Buffer size for transmission used in the shell communication
+#define UART_RX_BUFFER_SIZE 1				///< Buffer size for reception used in the shell communication
+#define CMD_BUFFER_SIZE 64					///< Buffer size for the shell command line
+#define MAX_ARGS 9							///< Maximum number of arguments for a command in the shell script
+					
+#define ASCII_CR 0x0D						///< Defines CR, carriage return command in the shell
+#define ASCII_DEL 0x7F						///< Defines DEL, delete command in the shell	
 
-#define SPEED_MAX 512 // Temporary value
+#define SPEED_MAX 512 						///< Arbitrary number representing out maximum value for the motor speed
 
-#define TIMCLOCK   170000000
-#define PRESCALAR  1
+#define TIMCLOCK   170000000				///< Definition of bus frequency for clock, useful to calculate time
+#define PRESCALAR  1						///< Definition of prescaler clock, useful to calculate time
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -62,52 +59,63 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-const uint8_t prompt[]="user@Nucleo-STM32G431>>";
-const uint8_t started[]=
+const uint8_t prompt[]="user@Nucleo-STM32G431>>";		///< Shell prompt text
+const uint8_t started[]=								///< Startup message when shell is initialized
 		"\r\n*-----------------------------*"
 		"\r\n| Welcome on Nucleo-STM32G431 |"
 		"\r\n*-----------------------------*"
 		"\r\n";
-const uint8_t newLine[]="\r\n";
-const uint8_t cmdNotFound[]="Command not found\r\n";
-const uint8_t help[]=
+const uint8_t newLine[]="\r\n";							///< Defines characters used to create a new line
+const uint8_t cmdNotFound[]="Command not found\r\n";	///< Shell message for when a user input command is not implemented
+const uint8_t help[]=									///< Shell message for when the user types "help"
 		"\r\n*-----------------------------*"
 		"\r\n| Help Menu |"
 		"\r\n*-----------------------------*"
-		"\r\n*Set PA5 1 : Turns ON the LED*"
-		"\r\n*Set PA5 0 : Turns OFF the LED*"
-		"\r\n*ETCCC*"
+		"\r\n*set PA5 1 : Turns ON the LED*"
+		"\r\n*set PA5 0 : Turns OFF the LED*"
+		"\r\n*get : Prints the current and the frequency in the motor*"
+		"\r\n*pinout : Prints the pinout list*"
+		"\r\n*start : Starts the motor*"
+		"\r\n*speed x : Sets the speed of the motor to x (-512 < x < 512)*"
 		"\r\n";
 
-const uint8_t pinout[]=
+const uint8_t pinout[]=									///< Shell message that lists the pinout list of the microcontroller
 		"\r\n*-----------------------------*"
 		"\r\n| Pinout List |"
 		"\r\n*-----------------------------*"
+		"\r\n*PA0: ADC_CURRENT*"		
 		"\r\n*PA5: LED*"
-		"\r\n*ETCCC*"
+		"\r\n*PA8: TIM1_CH1*"
+		"\r\n*PA9: TIM1_CH2*"
+		"\r\n*PA11: TIM1_CH1N*"
+		"\r\n*PA12: TIM1_CH2N*"
+		"\r\n*PC3: ISO_RESET*"
 		"\r\n";
-const uint8_t powerOn[]=
+const uint8_t powerOn[]=								///< Shell message when powering on the motor
 		"\r\n*-----------------------------*"
 		"\r\n| Motor ON |"
 		"\r\n";
-const uint8_t powerOff[]=
+const uint8_t powerOff[]=								///< Shell message when powering off the motor
 		"\r\n*-----------------------------*"
 		"\r\n| Motor OFF |"
 		"\r\n";
 
-const char * separators = " =";
+const char * separators = " =";							///< List of separators used to parse the strings in shell
 
-uint32_t uartRxReceived;
-uint8_t uartRxBuffer[UART_RX_BUFFER_SIZE];
-uint8_t uartTxBuffer[UART_TX_BUFFER_SIZE];
+uint32_t uartRxReceived;								///< Set to 1 when a new character is received on uart 2
+uint8_t uartRxBuffer[UART_RX_BUFFER_SIZE];				///< A buffer to store the received data from UART
+uint8_t uartTxBuffer[UART_TX_BUFFER_SIZE];				///< A buffer to store the UART data to be transmitted
 
-uint32_t IC_Val1 = 0;
-uint32_t IC_Val2 = 0;
-uint32_t Difference = 0;
-int Is_First_Captured = 0;
+
+uint32_t IC_Val1 = 0;									///< First timer value
+uint32_t IC_Val2 = 0;									///< Second timer value
+uint32_t Difference = 0;								///< Difference of the two timer values, to be able to know the direction of the rotation
+
+int Is_First_Captured = 0;								///< Set to 1 when the first rising edge is captured
+int Is_First_Captured_2 = 0;							///< Set to 1 when the first rising edge is captured (version 2)
 
 /* Measure Frequency */
-float frequency = 0;
+float frequency = 0;									///< Declaration of variable to hold the frequency value
 
 /* USER CODE END PV */
 
@@ -129,24 +137,17 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	char	 	cmd[CMD_BUFFER_SIZE];
-	int 		idxCmd;
-	char* 		argv[MAX_ARGS];
-	int		 	argc = 0;
-	char*		token;
-	int 		newCmdReady = 0;
-	int		speed = 0;
-	uint16_t  CCR1 = 5312;
-	uint16_t  CCR2 = 5312;
+	char	 	cmd[CMD_BUFFER_SIZE];		///< Initializes a cmd list to store the characters sent via shell
+	int 		idxCmd;						///< Initializes an index used to parse through the shell characters
+	char* 		argv[MAX_ARGS];				///< Initializes a variable to hold the arguments of a function
+	int		 	argc = 0;					///< Initializes a variable to hold the argc value
+	char*		token;						///< Initializes a variable to hold the tokens from strtok function
+	int 		newCmdReady = 0;			///< Initializes a variable to verify if shell has finished user input
+	int		speed = 0;						///< Initializes a variable to hold the speed value
+	uint16_t  CCR1 = 5312;					///< Initializes a variable to hold the value in the compare register channel 1
+	uint16_t  CCR2 = 5312;					///< Initializes a variable to hold the value in the compare register channel 2
 
-	uint16_t AD_RES = 0;
-
-	/*
-	const uint8_t help[] : contenant le message d'aide, la liste des fonctions
-	const uint8_t pinout[] : contenant la liste des pin utilisées
-	const uint8_t powerOn[] : contenant le message d'allumage du moteur
-	const uint8_t powerOff[] : contenant le message d'extinction du moteur
-	 */
+	uint16_t AD_RES = 0;					///< Initializes a variable to hold the value form the ADC conversion for the current
 
   /* USER CODE END 1 */
 
@@ -179,7 +180,7 @@ int main(void)
 	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
 
 	HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
-	HAL_TIM_IC_Start(&htim2, TIM_CHANNEL_2);
+	HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_2);
 
 	memset(argv,NULL,MAX_ARGS*sizeof(char*));
 	memset(cmd,NULL,CMD_BUFFER_SIZE*sizeof(char));
@@ -228,6 +229,7 @@ int main(void)
 
 
 		if(newCmdReady){
+			// Traiter l'instruction correspondante
 			if(strcmp(argv[0],"set")==0){
 				if(strcmp(argv[1],"PA5")==0){
 					HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, atoi(argv[2]));
@@ -242,7 +244,6 @@ int main(void)
 			{
 				// Read The ADC Conversion Result & Map It To PWM DutyCycle
 				AD_RES = ((3300*HAL_ADC_GetValue(&hadc1)/4096)-2500)*12;
-				//AD_RES = ((HAL_ADC_GetValue(&hadc1)*3300)/4096-2500)*12;
 				sprintf(uartTxBuffer,"ADC = %d | Freq = %d \r\n",AD_RES,(int) frequency);
 				HAL_UART_Transmit(&huart2, uartTxBuffer, sizeof(uartTxBuffer), HAL_MAX_DELAY);
 
@@ -280,6 +281,15 @@ int main(void)
 			HAL_UART_Transmit(&huart2, prompt, sizeof(prompt), HAL_MAX_DELAY);
 			newCmdReady = 0;
 		}
+
+		/*
+		if ((Is_First_Captured-Is_First_Captured_2)==1){
+
+		}
+		if (Is_First_Captured!=0 || Is_First_Captured_2!=0){
+			if
+		}
+		*/
 
 		// Start ADC Conversion
 		HAL_ADC_Start(&hadc1);
@@ -343,12 +353,18 @@ void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart){
 	HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
 }
 
+/**
+ * Sends the required sequence to power up the motor
+ * @param[in]  None
+ * @param[out] None
+ */
 void powerUpSequence (void){
 	HAL_GPIO_WritePin(ISO_RESET_GPIO_Port, ISO_RESET_Pin, GPIO_PIN_SET);
 	HAL_Delay(1);
 	HAL_GPIO_WritePin(ISO_RESET_GPIO_Port, ISO_RESET_Pin, GPIO_PIN_RESET);
 }
 
+/*
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 	if (htim->Instance == TIM2) {
 		uint32_t cl = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
@@ -356,6 +372,53 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 
 		frequency = (float) TIMCLOCK / (cl + 1);
 	}
+}
+*/
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+	{
+		if (Is_First_Captured==0) // if the first rising edge is not captured
+		{
+			IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // read the first value
+			Is_First_Captured = 1;  // set the first captured as true
+		}
+
+		else   // If the first rising edge is captured, now we will capture the second edge
+		{
+			IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);  // read second value
+
+			if (IC_Val2 > IC_Val1)
+			{
+				Difference = IC_Val2-IC_Val1;
+			}
+
+			else if (IC_Val1 > IC_Val2)
+			{
+				Difference = (0xffffffff - IC_Val1) + IC_Val2;
+			}
+
+			float refClock = TIMCLOCK/(PRESCALAR);
+
+			frequency = refClock/Difference;
+
+			__HAL_TIM_SET_COUNTER(htim, 0);  // reset the counter
+			Is_First_Captured = 0; // set it back to false
+		}
+	}
+	else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2)
+	{
+			if (Is_First_Captured_2==0) // if the first rising edge is not captured
+			{
+				Is_First_Captured_2 = 1;  // set the first captured as true
+			}
+
+			else   // If the first rising edge is captured, now we will capture the second edge
+			{
+				Is_First_Captured_2 = 0; // set it back to false
+			}
+		}
 }
 /* USER CODE END 4 */
 
--- TP_actionneurs/Core/Src/tim.c
+++ TP_actionneurs/Core/Src/tim.c
@@ -117,9 +117,8 @@ void MX_TIM2_Init(void)
   /* USER CODE END TIM2_Init 0 */
 
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
-  TIM_IC_InitTypeDef sConfigIC = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
 
   /* USER CODE BEGIN TIM2_Init 1 */
 
@@ -143,12 +142,9 @@ void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_RESET;
-  sSlaveConfig.InputTrigger = TIM_TS_TI1FP1;
-  sSlaveConfig.TriggerPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
-  sSlaveConfig.TriggerPrescaler = TIM_ICPSC_DIV1;
-  sSlaveConfig.TriggerFilter = 0;
-  if (HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK)
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
@@ -160,15 +156,7 @@ void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_FALLING;
-  sConfigIC.ICSelection = TIM_ICSELECTION_INDIRECTTI;
   if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
@@ -203,9 +191,10 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**TIM2 GPIO Configuration
+    PA1     ------> TIM2_CH2
     PA15     ------> TIM2_CH1
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_15;
+    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_15;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -274,9 +263,10 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
     __HAL_RCC_TIM2_CLK_DISABLE();
 
     /**TIM2 GPIO Configuration
+    PA1     ------> TIM2_CH2
     PA15     ------> TIM2_CH1
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_15);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1|GPIO_PIN_15);
 
     /* TIM2 interrupt Deinit */
     HAL_NVIC_DisableIRQ(TIM2_IRQn);
